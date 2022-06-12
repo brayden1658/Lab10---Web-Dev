ed3769fe6779ee8b43f1a41f378c00847736356b
brayden1658 <braydenw054@gmail.com>
2022-06-12 18:53:27 +1200

    Initial commit

---
+++ src/app/course.ts
@@ -0,0 +1,7 @@
+export class Course {
+  course_id!: number;
+  course_title!: string;
+  semester!: string;
+  period!: string;
+  lecturer!: string;
+}
---
+++ src/app/test-course.ts
@@ -0,0 +1,10 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+  { course_id: 1, course_title: "Web Development", semester: "One", period: "Wednesday 4-6 PM", lecturer: "Dr. Jian Yu"},
+  { course_id: 2, course_title: "Operating Systems", semester: "One", period: "Monday 8-10 AM", lecturer: "Dr. Edmund Lai"},
+  { course_id: 3, course_title: "R & D", semester: "One", period: "Tuesday", lecturer: "Dr Ramesh Lal"},
+  { course_id: 4, course_title: "Contemp Issues in Software Engineering", semester: "One", period: "Wednesday 8-10 AM", lecturer: "Tony Clear"},
+  { course_id: 5, course_title: "Advanced Network Technologies", semester: "Two", period: "Tuesday 4-6 PM", lecturer: ""},
+
+];
