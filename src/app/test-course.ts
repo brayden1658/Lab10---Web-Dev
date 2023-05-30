d18d1a76f7e210e85c4fba50844263c3ac70c43d
brayden1658 <74026978+brayden1658@users.noreply.github.com>
2023-05-30 14:40:14 +1200

    2 files modified

--- src/app/test-course.ts
+++ src/app/test-course.ts
@@ -1,10 +1,32 @@
 import { Course } from './course';
 
 export const COURSES: Course[] = [
-  { course_id: 1, course_title: "Web Development", semester: "One", period: "Wednesday 4-6 PM", lecturer: "Dr. Jian Yu"},
-  { course_id: 2, course_title: "Operating Systems", semester: "One", period: "Monday 8-10 AM", lecturer: "Dr. Edmund Lai"},
-  { course_id: 3, course_title: "R & D", semester: "One", period: "Tuesday", lecturer: "Dr Ramesh Lal"},
-  { course_id: 4, course_title: "Contemp Issues in Software Engineering", semester: "One", period: "Wednesday 8-10 AM", lecturer: "Tony Clear"},
-  { course_id: 5, course_title: "Advanced Network Technologies", semester: "Two", period: "Tuesday 4-6 PM", lecturer: ""},
-
+  {
+    course_id: 1,
+    course_title: 'Web Development',
+    semester: 'Semester 1 2023',
+    period: 'Tuesdays',
+    lecturer: 'Dr Jian Yu',
+  },
+  {
+    course_id: 2,
+    course_title: 'Artifical Intelligence',
+    semester: 'Semester 1 2023',
+    period: 'Mondays',
+    lecturer: 'Dr Ji Ruan',
+  },
+  {
+    course_id: 2,
+    course_title: 'Logic and Discrete Structures',
+    semester: 'Semester 1 2023',
+    period: 'Fridays',
+    lecturer: 'Sira Yongchareon',
+  },
+  {
+    course_id: 2,
+    course_title: 'Data Structures and Algorithms',
+    semester: 'Semester 1 2023',
+    period: 'Thursdays',
+    lecturer: 'Dr. Xinyu Hu',
+  },
 ];
